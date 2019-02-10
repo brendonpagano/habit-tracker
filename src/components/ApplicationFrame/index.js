e40ea2a6a70d68cb12b205f83803f6ca87877858
Brendon Pagano <brendonpagano@gmail.com>
2019-02-10 02:51:40 -0200

    Create ApplicationFrame component

---
+++ src/components/ApplicationFrame/index.js
@@ -0,0 +1,123 @@
+// Node Modules
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+// Utils
+import withStyle from './styles';
+
+
+class ApplicationFrame extends PureComponent {
+
+  state = {
+    mobileOpen: false,
+  };
+
+  onDrawerToggle = () => {
+    this.setState((prevState) => ({ mobileOpen: !prevState.mobileOpen }));
+  };
+
+
+  renderDrawer = (classes) => (
+    <div>
+      <div className={classes.toolbar} />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  render() {
+    const { classes, theme, children } = this.props;
+    const { mobileOpen } = this.state;
+
+    const drawer = this.renderDrawer(classes);
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position='fixed' className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color='inherit'
+              aria-label='Open Drawer'
+              onClick={this.onDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' color='inherit' noWrap>
+              Habit Tracker
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <nav className={classes.drawer}>
+          <Hidden smUp implementation='css'>
+            <Drawer
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={mobileOpen}
+              onClose={this.onDrawerToggle}
+              classes={{ paper: classes.drawerPaper }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{ paper: classes.drawerPaper }}
+              variant="permanent"
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          {children}
+        </main>
+      </div>
+    );
+  }
+
+}
+
+
+ApplicationFrame.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+
+export default withStyle(ApplicationFrame);
---
+++ src/components/ApplicationFrame/styles.js
@@ -0,0 +1,41 @@
+// Node Modules
+import { withStyles } from '@material-ui/core';
+
+// Constants
+const DRAWER_WIDTH = 240;
+
+
+const applicationFrameStyles = (theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: DRAWER_WIDTH,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: DRAWER_WIDTH,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: DRAWER_WIDTH,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+
+export default withStyles(applicationFrameStyles, { withTheme: true });
