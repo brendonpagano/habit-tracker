036f7118430fe5fa80713995b31d961fb6ad5082
Brendon Pagano <brendonpagano@gmail.com>
2019-02-10 00:58:52 -0200

    Create redux setup

---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+// Node Modules
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+
+// Inner Modules
+import createRootReducer from './root_reducer';
+
+
+// Add support to Redux Dev Tools by changing the compose function if present.
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Get the history object, to be used across the application.
+const history = createBrowserHistory();
+
+
+// Create application middlewares.
+const middlewares = [
+  routerMiddleware(history)
+];
+
+
+// Create the redux store.
+const store = createStore(
+  createRootReducer(history),
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+
+export default { store, history };
