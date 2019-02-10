f4c62acfdf24650ba2228b3d7f545b766e98036b
Brendon Pagano <brendonpagano@gmail.com>
2019-02-10 03:01:16 -0200

    Separate the drawer rendering from the ApplicationFrame

--- src/components/ApplicationFrame/index.js
+++ src/components/ApplicationFrame/index.js
@@ -5,19 +5,13 @@ import PropTypes from 'prop-types';
 // Components
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import ApplicationDrawer from '../ApplicationDrawer';
 
 // Utils
 import withStyle from './styles';
@@ -34,34 +28,11 @@ class ApplicationFrame extends PureComponent {
   };
 
 
-  renderDrawer = (classes) => (
-    <div>
-      <div className={classes.toolbar} />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
   render() {
     const { classes, theme, children } = this.props;
     const { mobileOpen } = this.state;
 
-    const drawer = this.renderDrawer(classes);
+    const drawer = <ApplicationDrawer />;
 
     return (
       <div className={classes.root}>
